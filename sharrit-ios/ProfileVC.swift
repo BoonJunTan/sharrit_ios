3092b475bebbaf3b3f4c52c30242f2b2e3adf023
Dyllan <Tan@Boons-MacBook-Pro.local>
2017-09-13 23:55:36 +0800

    Done with profile page UI

--- sharrit-ios/ProfileVC.swift
+++ sharrit-ios/ProfileVC.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Cosmos
 
 class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     let tableViewSection = ["", "SETTINGS"]
-    //let tableViewIcons = [[], []]
-    let tableViewitems = [["My Sharres", "Reputation"], ["Profile Settings", "Help Centre", "View as Sharror", "Logout"]]
+    let tableViewIcons = [[#imageLiteral(resourceName: "Sharrit_Logo"), #imageLiteral(resourceName: "reputation")], [#imageLiteral(resourceName: "profile2"), #imageLiteral(resourceName: "help"), #imageLiteral(resourceName: "change_role"), #imageLiteral(resourceName: "logout")]]
+    let tableViewItems = [["My Sharres", "Reputation"], ["Profile Settings", "Help Centre", "View as Sharror", "Logout"]]
+
+    @IBOutlet weak var starRating: CosmosView!
+    let fakeRatingDouble = 4.7
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +27,12 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                                            target: self, action: #selector(goToMessages))
         
         self.navigationItem.rightBarButtonItem = navBarBubble
+        starRating.rating = fakeRatingDouble
+        starRating.settings.fillMode = .precise
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.tableFooterView = UIView() // For Hiding away empty cell
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,12 +40,27 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return tableViewSection[section]
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let headerHeight:CGFloat = tableViewSection[section].isEmpty ? 0.0 : 50.0
+        return headerHeight
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return tableViewItems.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return tableViewItems[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "profileCell") as! ProfileTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell") as! ProfileTableViewCell
+        cell.iconLabel.text = tableViewItems[indexPath.section][indexPath.row]
+        cell.iconImage.image = tableViewIcons[indexPath.section][indexPath.row]
         return cell
     }
     
