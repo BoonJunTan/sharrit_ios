f1c95bb05ee131a80983a590900e48c66b610e67
Dyllan <>
2017-10-13 19:56:20 +0800

    Solved bug for the day

--- sharrit-ios/SharreTimeUsageVC.swift
+++ sharrit-ios/SharreTimeUsageVC.swift
@@ -96,8 +96,6 @@ class SharreTimeUsageVC: UIViewController {
     }
     
     @IBAction func bookBtnPressed(_ sender: SharritButton) {
-        self.performSegue(withIdentifier: "viewSuccessful", sender: nil)
-        
         let deposit = depositLabel.text!.replacingOccurrences(of: "Deposit: $", with: "")
         
         let url = SharritURL.devURL + "transaction/" + String(describing: sharreID!)
